b96f2d28b588a16d1ef0ef7e7fa9e9619d8fcbad
lcyWay <carepuw@gmail.com>
2024-04-13 14:46:33 +0300

    feat: remove chairs project web link

--- src/views/ChairsProjectView/index.tsx
+++ src/views/ChairsProjectView/index.tsx
@@ -9,7 +9,6 @@ function ChairsProjectView() {
       <ProjectCard
         title="Chairs Market"
         githubLink="https://github.com/lcyWay/chairs-market"
-        webLink="https://chairs-market.vercel.app"
         description="This is a simple gaming chair store. But the project is written on a non-primitive stack and is also covered with unit and e2e tests. The production uses a free server, so the time before the first download of the assortment of chairs may be delayed."
         stack="Docker, Redis, PostgreSQL, Node.js, Express, Webpack, Typescript, React, MobX, Vanilla Extract, Loadable Components, Prettier, Eslint, Jest, Cypress, Husky, Lint-Staged"
         imageSrc="/images/chairs.png"
